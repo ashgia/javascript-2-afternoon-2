8390c082014cbce7a056541a073c26bbad43794a
Ashley Nguyen <ashleygnguyen@yahoo.com>
2018-08-07 14:50:31 -0500

    first edit

--- practice.js
+++ practice.js
@@ -18,7 +18,11 @@ var arr = [10,20,30];
 */
 
 //Code Here
+function first(arr){
+  return arr[0]
+}
 
+first(arr)
 
 
 ////////// PROBLEM 2 //////////
@@ -32,7 +36,11 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last(arr){
+  return arr[arr.length-1]
+}
+
+last(arr)
 
 
 
@@ -48,7 +56,13 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
+function looper(family){
+  for(var i=0;i<family.length;i++){
+    alert (family[i])
+  }
+}
 
+looper(family)
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +77,12 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for(i=letters.length-1;i>=0;--i){
+    alert (letters[i])
+  }
+}
+reversedLooper(letters)
 
 
 ////////// PROBLEM 5 //////////
@@ -78,8 +97,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+function evenFinder(nums){
+  let newArray = nums.filter(function(num){
+    return num % 2 === 0;
+  })
+  return newArray;
+}
 
-
+evenFinder(nums)
 
 
 
